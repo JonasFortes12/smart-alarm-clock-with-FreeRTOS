b896f42bad09c37573a8eb2aee2036cfb69da97a
jaopaulomilitao <jaopaulomilitao@gmail.com>
2024-09-12 20:49:59 -0300

    fix: initial tasks conflicts solved

--- main/libs/services.cpp
+++ main/libs/services.cpp
@@ -7,14 +7,14 @@
 #include "free-rtos-source/include/FreeRTOS.h"
 
 // create TaskHandle_t for identify the ring alarm task
-TaskHandle_t ringAlarmTaskHandle = NULL;  
+TaskHandle_t ringAlarmTaskHandle = NULL;
 
 // Create the queue for the noise sensor
 QueueHandle_t noiseQueue = NULL;
 
+void setup_modules_services()
+{
 
-void setup_modules_services()  {
-    
     // Initialize Serial communication
     Serial.begin(9600);
 
@@ -36,96 +36,119 @@ void setup_modules_services()  {
     setup_pir();
 }
 
-void update_time_on_lcd_task(void *pvParameters) {
-    (void) pvParameters;
-    for (;;) {
-        now = rtc.now();  // Atualiza o tempo do RTC
+void update_time_on_lcd_task(void *pvParameters)
+{
+    (void)pvParameters;
+    for (;;)
+    {
+        now = rtc.now(); // Atualiza o tempo do RTC
         String time = get_time();
         String date = get_date();
         clearLCD();
-        showMessage(time, 0); 
+        showMessage(time, 0);
         showMessage(date, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Wait for 1 second
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Wait for 1 second
     }
 }
 
-void ring_alarm_task(void *pvParameters) {
-    (void) pvParameters;
+void ring_alarm_task(void *pvParameters)
+{
+    (void)pvParameters;
     bool noiseDetected;
     alarm_ringing = true; // Set the alarm as ringing
-    
-    while (!is_pushed_stop_button()) {
+
+    while (!is_pushed_stop_button())
+    {
         // Receives the noiseDetected value from the queue
-        if (xQueueReceive(noiseQueue, &noiseDetected, 0)) {
-            if (noiseDetected) {
-                ring_hard_sound();  // If there is noise, ring the hard sound
-            } else {
-                ring_soft_sound();  // If there is no noise, ring the soft sound
+        if (xQueueReceive(noiseQueue, &noiseDetected, 0))
+        {
+            if (noiseDetected)
+            {
+                ring_hard_sound(); // If there is noise, ring the hard sound
             }
-        } else {
+            else
+            {
+                ring_soft_sound(); // If there is no noise, ring the soft sound
+            }
+        }
+        else
+        {
             // If the queue is empty, ring the soft sound
             ring_soft_sound();
         }
     }
-    
-    stop_sound(); // Stop the alarm
-    clear_alarm();  // Clear the alarm set
-    alarm_ringing = false;  // Set the alarm as not ringing
-    vTaskDelete(NULL);  // close this task
+
+    stop_sound();          // Stop the alarm
+    clear_alarm();         // Clear the alarm set
+    alarm_ringing = false; // Set the alarm as not ringing
+    vTaskDelete(NULL);     // close this task
 }
 
-void verify_alarm_time_task(void *pvParameters) {
-    (void) pvParameters;
-    for (;;) {
+void verify_alarm_time_task(void *pvParameters)
+{
+    (void)pvParameters;
+    for (;;)
+    {
         // Get the current time
         now = rtc.now();
         // If the alarm time is defined and the current time is equal to the alarm time, create the ring alarm task
-        if (is_alarm_time() && ringAlarmTaskHandle == NULL) {
+        if (is_alarm_time() && ringAlarmTaskHandle == NULL)
+        {
             xTaskCreate(ring_alarm_task, "Ring Alarm", 2048, NULL, 2, &ringAlarmTaskHandle);
+            // showMessage("Alarm ringing", 1);
         }
         // If the stop button is pressed and the alarm is not ringing, clear the alarm
-        if(is_pushed_stop_button() && !alarm_ringing) {
-            clear_alarm();  // Clear the alarm set
+        if (is_pushed_stop_button() && !alarm_ringing)
+        {
+            clear_alarm(); // Clear the alarm set
         }
 
-        vTaskDelay(500 / portTICK_PERIOD_MS);  // Verify every 500ms
+        vTaskDelay(500 / portTICK_PERIOD_MS); // Verify every 500ms
     }
 }
 
-void verify_moviment_task(void *pvParameters) {
-    (void) pvParameters;
-    for (;;) {
-        if (is_moviment_detected() && alarm_ringing) {
-            set_snooze_time(5);  // add 5 minutes to the snooze time
-            stop_sound();  // stop the alarm
-            alarm_ringing = false;  // set the alarm as not ringing
-            if (ringAlarmTaskHandle != NULL) {
-                vTaskDelete(ringAlarmTaskHandle);  // Delete the ringing task
-                ringAlarmTaskHandle = NULL; 
+void verify_moviment_task(void *pvParameters)
+{
+    (void)pvParameters;
+    for (;;)
+    {
+        if (is_moviment_detected() && alarm_ringing)
+        {
+            set_snooze_time(5);    // add 5 minutes to the snooze time
+            stop_sound();          // stop the alarm
+            alarm_ringing = false; // set the alarm as not ringing
+            if (ringAlarmTaskHandle != NULL)
+            {
+                vTaskDelete(ringAlarmTaskHandle); // Delete the ringing task
+                ringAlarmTaskHandle = NULL;
             }
         }
-        vTaskDelay(200 / portTICK_PERIOD_MS);  // Verifica a cada 200ms
+        vTaskDelay(200 / portTICK_PERIOD_MS); // Verifica a cada 200ms
     }
 }
 
-void verify_noise_task(void *pvParameters) {
-     (void) pvParameters;
+void verify_noise_task(void *pvParameters)
+{
+    (void)pvParameters;
     bool noiseDetected;
-    for (;;) {
-        noiseDetected = is_noise_detected(); 
+    for (;;)
+    {
+        noiseDetected = is_noise_detected();
         xQueueSend(noiseQueue, &noiseDetected, portMAX_DELAY); // send the noiseDetected value to the queue
-        vTaskDelay(200 / portTICK_PERIOD_MS);  // vefiry every 200ms
+        vTaskDelay(200 / portTICK_PERIOD_MS);                  // vefiry every 200ms
     }
 }
 
-void log(void *pvParameters) {
-    (void) pvParameters;
-    for (;;) {
+void log(void *pvParameters)
+{
+    (void)pvParameters;
+    for (;;)
+    {
         Serial.println("Moviment: " + String(is_moviment_detected()));
         Serial.println("Noise: " + String(is_noise_detected()));
         Serial.println("Button: " + String(is_pushed_stop_button()));
         Serial.println("Alarm Time Defined: " + String(alarm_hour) + ":" + String(alarm_minute) + ":" + String(alarm_second));
         Serial.println("-----------------------------------------");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Wait for 1000ms
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Wait for 1000ms
     }
 }
